fa24f2b8b24376d8c848994c53c76abc3a762b12
iiiina <seodew@gmail.com>
2026-04-24 01:57:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,33 @@
 import requests
 import os
 
+print("API 테스트 시작")
+
 APP_KEY = os.getenv("KIS_APP_KEY")
 APP_SECRET = os.getenv("KIS_APP_SECRET")
 
-# 토큰 발급
+print("APP_KEY:", APP_KEY is not None)
+print("APP_SECRET:", APP_SECRET is not None)
+
 url = "https://openapi.koreainvestment.com:9443/oauth2/tokenP"
-headers = {"content-type": "application/json"}
+
+headers = {
+    "content-type": "application/json"
+}
+
 data = {
     "grant_type": "client_credentials",
     "appkey": APP_KEY,
     "appsecret": APP_SECRET
 }
+
 res = requests.post(url, headers=headers, json=data)
-ACCESS_TOKEN = res.json()["access_token"]
 
-print("토큰 발급 완료")
-
-# 현재가 조회
-url = "https://openapi.koreainvestment.com:9443/uapi/domestic-stock/v1/quotations/inquire-price"
-
-headers = {
-    "authorization": f"Bearer {ACCESS_TOKEN}",
-    "appkey": APP_KEY,
-    "appsecret": APP_SECRET,
-    "tr_id": "FHKST01010100"
-}
-
-params = {
-    "fid_cond_mrkt_div_code": "J",
-    "fid_input_iscd": "005930"
-}
-
-res = requests.get(url, headers=headers, params=params)
-
+print("응답 전체 ↓↓↓")
 print(res.json())
+
+if "access_token" in res.json():
+    ACCESS_TOKEN = res.json()["access_token"]
+    print("토큰 발급 성공")
+else:
+    print("토큰 없음 → 위 에러 확인해라")
